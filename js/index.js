9f154256cd8652392d78dd39bd9de41d8ad9b9b0
user-PC\user <j_mizobe@yahoo.co.jp>
2017-05-29 21:26:08 +0900

    Validate

--- js/index.js
+++ js/index.js
@@ -12,6 +12,38 @@
 		},
 		
 		'input, textarea focusout': function(context, $el) {
+                      //変数の定義
+                      var value = $el.val(); //
+                      var name = $el.attr('name'); //
+                      var error_class = 'has-error'; //
+                      var $msg = this.$find('.report-content').find('.msg'); //
+                      var $formGroup = $el.parents('.form-group'); //
+                      //???O????e
+                      if (name == 'img') {
+                         return;
+                      }
+                      //?u??`?F?b?N
+                      if (value == null || value == '') {
+                          //?u??3?e??￠??￠???????
+                          if ($formGroup.hasClass(error_class)) {
+                             return;
+                          }
+                          $formGroup.addClass(error_class);
+                          var label = $formGroup.find('label').text();
+                          var $p = $('<p data-handson-input-name="' + name +'">');
+                          $p.append('<strong>' + label + 'を入力してください' + '</strong>');
+                          $msg.append($p);
+                      } else {
+                          //?u??3?e??￠?e???????
+                          $formGroup.removeClass(error_class);
+                          $msg.find(p[data-handson-input-name="' + name + '"]).remove();
+                      }
+                      //???b?Z?[?W??\?|?A?n?\?|??w?e
+                      if ($msg.children().length != 0) {
+                          $msg.show();
+                      } else {
+                          $msg.hide();
+                      }
 		},
 
 		'input[name="img"] change': function(context, $el) {
